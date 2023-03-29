e1bb74571ace8125b593631f386d50c90eda54c9
radheyjhorar <shyamjhorar700@gmail.com>
2023-03-29 17:58:16 +0530

    fixed

--- frontend/src/pages/customer/NewCustomer.jsx
+++ frontend/src/pages/customer/NewCustomer.jsx
@@ -8,25 +8,27 @@ const NewCustomer = () => {
 
   return (
     <>
-      <div class="col-7 c-7-d mx-auto bg-pic">
-        <h1 className="offset-2 h4 my-apk-clr mt-5">New Customer</h1>
-        <form action="">
-          <div className="text-center">
-            <input type="text" className="col-4 vndr-ipt my-4 d-inline" placeholder="Customer Name" />
-            <input type="number" minlength="10" maxlength="10" className="col-4 vndr-ipt my-4 d-inline" placeholder="Mobile 1" />
-            <input type="text" className="col-4 vndr-ipt d-block" placeholder="City" />
-            <input type="number" minlength="10" min="10" max="10" maxlength="10" className="col-4 vndr-ipt d-block" placeholder="Mobile 2" />
-            <input type="text" className="col-8 my-4 vndr-ipt d-block" placeholder="State" />
-            <input type="text" className="col-8 mb-4 vndr-ipt d-block" placeholder="Customer Address" />
-            <input type="number" minlength="10" maxlength="10" className="col-8 vndr-ipt mb-5 d-block" placeholder="Ledger No." />
+     <div class="col-7 c-7-d mx-auto bg-pic">
+                <h1 class="offset-2 h4 my-apk-clr mt-5">New Customer</h1>
 
-            <div className="">
-              <button type="submit" className="btn sbmt-btn px-4 mb-5 text-white my-apk-clr-bg text-center">Submit</button>
+                    <div class="text-center">
+                      <div>
+                        <input type="text" class="col-4 vndr-ipt my-4 d-inline-block" placeholder="Customer Name" />
+                        <input type="number" minlength="10" maxlength="10" class="col-4 vndr-ipt my-4 d-inline-block" placeholder="Mobile 1" />
+                        </div>
+                        <input type="text" class="col-4 vndr-ipt d-inline-block" placeholder="City" />
+                        <input type="number" minlength="10" min="10" max="10" maxlength="10" class="col-4 vndr-ipt d-inline-block" placeholder="Mobile 2" />
+                        <input type="text" class="col-8 my-4 vndr-ipt d-inline-block" placeholder="State" />
+                        <input type="text" class="col-8 mb-4 vndr-ipt d-inline-block" placeholder="Customer Address" />
+                        <input type="number" minlength="10" maxlength="10" class="col-8 vndr-ipt mb-5 d-inline-block" placeholder="Ledger No." />
+                        
+                        <div class="">
+                            <button type="submit" class="btn sbmt-btn px-4 mb-5 text-white my-apk-clr-bg text-center">Submit</button>
+                        </div>
+                    </div>
+                   
+           
             </div>
-          </div>
-
-        </form>
-      </div>
     </>
   );
 }
